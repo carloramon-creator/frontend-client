9c0dc48e1f5863cf0193cf25440a3ab621e5476c
Carlo Ramon <carloramon@gmail.com>
2026-01-15 12:20:54 -0300

    chore: update manifest and sw to force deploy

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+// Firebase Cloud Messaging Service Worker v2.0
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
